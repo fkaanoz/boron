bf3e3e6986a5c1c9354f9515fd9c050e009aafe6
fkaanoz <fkaan1997@gmail.com>
2025-08-31 14:51:43 +0300

    round permuation layer is added for dec & enc

--- hdl/decrytion/permutation/dec_round_permutation.v
+++ hdl/decrytion/permutation/dec_round_permutation.v
@@ -1,7 +1,27 @@
 `timescale 1ns/1ps
 
 
-module dec_round_permutation();
+module dec_round_permutation(
+    input [63:0] dec_rp_in,
+    output [63:0] dec_rp_out
+);
+
+
+
+// circular right shift by 9
+assign dec_rp_out[63:48] = {dec_rp_in[56:48], dec_rp_in[63:57]};
+
+
+// circular right shift by 7
+assign dec_rp_out[47:32] = {dec_rp_in[38:32], dec_rp_in[47:39]};
+
+
+// circular right shift by 4
+assign dec_rp_out[31:16] = {dec_rp_in[19:16], dec_rp_in[31:20]};
+
+
+// circular right shift by 1
+assign dec_rp_out[15:0] = {dec_rp_in[0], dec_rp_in[15:1]};
 
 
 endmodule
--- hdl/encrytion/permutation/round_permutation.v
+++ hdl/encrytion/permutation/round_permutation.v
@@ -1,7 +1,27 @@
 `timescale 1ns/1ps
 
 
-module round_permutation();
+module round_permutation(
+    input [63:0] rp_in,
+    output [63:0] rp_out
+);
+
+
+// circular left shift by 9
+assign rp_out[63:48] = {rp_in[54:48], rp_in[63:55]};
+
+
+// circular left shift by 7
+assign rp_out[47:32] = {rp_in[40:32], rp_in[47:41]};
+
+
+// circular left shift by 4
+assign rp_out[31:16] = {rp_in[27:16], rp_in[31:28]};
+
+
+// circular left shift by 1
+assign rp_out[15:0] = {rp_in[14:0], rp_in[15]};
+
 
 
 endmodule
