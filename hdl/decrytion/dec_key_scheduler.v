c02469905a8e58ec37d80a5aa208261967e5ee87
fkaanoz <fkaan1997@gmail.com>
2025-08-31 15:30:14 +0300

    key scheduler for enc & dec added

--- hdl/decrytion/dec_key_scheduler.v
+++ hdl/decrytion/dec_key_scheduler.v
@@ -1,8 +1,21 @@
 `timescale 1ns/1ps
 
 
-module dec_key_scheduler();
+module dec_key_scheduler(
+    input [4:0] round_counter,
+    input [79:0] KEY,
+    output [79:0] PREV_KEY
+);
 
 
+wire [3:0] temp1;
+dec_sbox s0(.x(KEY[3:0]), .y(temp1));
+
+
+wire [4:0] temp2;
+assign temp2 = KEY[63:59] ^ round_counter;
+
+
+assign PREV_KEY = {KEY[12:4], temp1, KEY[79:64], temp2, KEY[58:13] }
 
 endmodule
--- hdl/encrytion/enc_key_scheduler.v
+++ hdl/encrytion/enc_key_scheduler.v
@@ -1,7 +1,19 @@
 `timescale 1ns/1ps
 
 
-module enc_key_scheduler();
+module enc_key_scheduler(
+    input [4:0] round_counter,
+    input [79:0] KEY,
+    output [79:0] NEXT_KEY
+);
+
+wire [3:0] temp1;
+sbox s0(.x(KEY[70:67]), .y(temp1));
+
+wire [4:0] temp2;
+assign temp2 = KEY[50:46] ^ round_counter;
+
+assign NEXT_KEY = {KEY[66:51], temp2, KEY[45:0], KEY[79:71], temp1};
 
 
 endmodule
