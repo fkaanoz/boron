540f63aad68786affbf5713bb666b22bb5eeb8c2
fkaanoz <fkaan1997@gmail.com>
2025-08-31 15:00:22 +0300

    xor operation layer is added for dec & enc

--- hdl/decrytion/permutation/dec_xor_operation.v
+++ hdl/decrytion/permutation/dec_xor_operation.v
@@ -1,7 +1,16 @@
 `timescale 1ns/1ps
 
 
-module dec_xor_operation();
+module dec_xor_operation(
+    input [63:0] dec_xor_in,
+    output [63:0] dec_xor_out
+);
+
+assign dec_xor_out[63:48] = xor_in[63:48] ^ xor_in[47:32];
+assign dec_xor_out[47:32] = xor_in[47:32] ^ xor_in[31:16] ^ xor_in[15:0];
+assign dec_xor_out[31:16] = xor_in[63:48] ^ xor_in[47:32] ^ xor_in[31:16];
+assign dec_xor_out[15:0] = xor_in[31:16] ^ xor_in[15:0];
+
 
 
 endmodule
--- hdl/encrytion/permutation/xor_operation.v
+++ hdl/encrytion/permutation/xor_operation.v
@@ -1,7 +1,16 @@
 `timescale 1ns/1ps
 
 
-module xor_operation();
+module xor_operation(
+    input [63:0] xor_in,
+    output [63:0] xor_out
+);
+
+
+assign xor_out[63:48] = xor_in[63:48] ^ xor_in[47:32] ^ xor_in[15:0];
+assign xor_out[47:32] = xor_in[47:32] ^ xor_in[15:0];
+assign xor_out[31:16] = xor_in[63:48] ^ xor_in[31:16];
+assign xor_out[15:0] = xor_in[63:48] ^ xor_in[31:16] ^ xor_in[15:0];
 
 
 endmodule
